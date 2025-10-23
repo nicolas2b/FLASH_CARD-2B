e7ea2f8639eb2042ab698bce8bcb37d37288f855
nicolas <santos.moraes.nicolas@escola.pr.gov.br>
2025-10-23 07:27:57 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,35 @@
+criaCartao(
+    'Basquete',
+    'Qual time tem mais títulos da NBA?',
+    'Boston Celtics'
+)
 
+criaCartao(
+    'Basquete',
+    'Quem é o maior pontuador da história da NBA?',
+    'Kareem Abdul-Jabbar'
+)
+
+criaCartao(
+    'Basquete',
+    'Quem é considerado o melhor jogador de basquete de todos os tempos?',
+    'Michael Jordan'
+)
+
+criaCartao(
+    'Basquete',
+    'Quantos pontos é uma cesta de 3 pontos na NBA?',
+    '3 pontos'
+)
+
+criaCartao(
+    'Basquete',
+    'Qual time tem mais títulos da NBA?',
+    'Los Angeles Lakers'
+)
+
+criaCartao(
+    'Basquete',
+    'Quem foi o MVP das finais da NBA em 2021?',
+    'Giannis Antetokounmpo'
+)
